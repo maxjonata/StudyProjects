d1856c4e2cc60380b138236f51f3ee5716629f36
JAMaxwell <max.kawaii@hotmail.com>
2019-01-08 07:36:28 -0200

    .

--- src/C/Uri/1011.c
+++ src/C/Uri/1011.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main()
@@ -9,16 +8,4 @@ int main()
     VOLUME = ((4.0/3.0) * pi * (R*R*R));
 
     printf("VOLUME = %.3lf\n", VOLUME);
-=======
-#include <stdio.h>
-
-int main()
-{
-    double pi = 3.14159, R, VOLUME;
-    scanf(" %lf", &R);
-    
-    VOLUME = ((4.0/3.0) * pi * (R*R*R));
-
-    printf("VOLUME = %.3lf\n", VOLUME);
->>>>>>> b40020655e623470d8694fef4dab9e11f5ddbd76
 }
