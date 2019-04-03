62ee2d365f3dd583effab1aadb20ea4946a23771
JAMaxwell <max.kawaii@hotmail.com>
2019-04-03 18:02:19 -0300

    .

--- src/java/3POB/Triangulo/src/br/edu/faeterj/Triangulo.java
+++ src/java/3POB/Triangulo/src/br/edu/faeterj/Triangulo.java
@@ -1,29 +1,34 @@
 package br.edu.faeterj;
 
 public class Triangulo {
+	Ponto p1,p2,p3;
 
 	public Triangulo(Ponto p1, Ponto p2, Ponto p3) {
 		// TODO Auto-generated constructor stub
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
 	}
 
 	public Ponto getP1() {
 		// TODO Auto-generated method stub
-		return new Ponto(0,0);
+		return p1;
 	}
 
 	public Ponto getP2() {
 		// TODO Auto-generated method stub
-		return new Ponto(0,0);
+		return p2;
 	}
 
 	public Ponto getP3() {
 		// TODO Auto-generated method stub
-		return new Ponto(0,0);
+		return p3;
 	}
 
 	public void setP3(Ponto ponto) {
 		// TODO Auto-generated method stub
 		
+		this.p3 = ponto;
 	}
 
 }
