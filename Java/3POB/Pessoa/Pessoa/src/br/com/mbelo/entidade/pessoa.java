9cee367bb7f43a6e360c9df78702ea538d616129
JAMaxwell <max.kawaii@hotmail.com>
2019-05-05 12:54:54 -0300

    .

--- Java/3POB/Pessoa/Pessoa/src/br/com/mbelo/entidade/pessoa.java
+++ Java/3POB/Pessoa/Pessoa/src/br/com/mbelo/entidade/pessoa.java
@@ -2,5 +2,59 @@ package br.com.mbelo.entidade;
 
 public class Pessoa
 {
-    
+    private String nome;
+    private int idade;
+    private char sexo;
+    private double altura;
+
+    public double calcularPesoIdeal()
+    {
+        return ((altura * altura) * 22.487654320987654320987654320988)
+    }
+
+    public String mostrarSaudacao()
+    {
+        return ("Ola " + nome);
+    }
+
+    public void setAltura(double altura)
+    {
+        this.altura = altura;
+    }
+
+    public void setSexo(char sexo)
+    {
+        this.sexo = sexo;
+    }
+
+    public void setIdade(int idade)
+    {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public double getAltura()
+    {
+        return altura;
+    }
+
+    public char getSexo()
+    {
+        return sexo;
+    }
+
+    public int getIdade()
+    {
+        return idade;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
 }
